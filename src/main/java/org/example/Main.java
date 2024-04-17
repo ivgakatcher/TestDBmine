cd3dfa04c0efc6a1b3a4237209a115e2bd1cea09
alkaz07 <alkaz07@yandex.ru>
2024-04-17 16:11:14 +0300

    +загрузка списка юзеров из БД

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,54 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Properties;
+import java.util.function.Consumer;
 
 public class Main {
-    public static void main(String[] args) {
-        example0();
+    public static void main(String[] args) throws SQLException {
+       // example0();
+        example1();
+        example2();
+    }
+
+    private static void example2() throws SQLException {
+        Connection conn = connectToDB();
+        ArrayList<User> users = loadUsers(conn);
+        System.out.println("получен список из "+users.size()+" юзеров");
+        users.forEach(System.out::println);
+
+        conn.close();
+    }
+
+    public static ArrayList<User> loadUsers(Connection conn) throws SQLException {
+        ArrayList<User> users=new ArrayList<>();
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM public.user");
+        while (rs.next()) {
+            int x = rs.getInt("id");
+            String fio = rs.getString("fio");
+            double m = rs.getDouble("money");
+            users.add(new User(x, fio, m));
+        }
+        rs.close();
+        st.close();
+        return users;
+    }
+
+    private static void example1() throws SQLException {
+        Connection conn = connectToDB();
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM public.user");
+        while (rs.next()) {
+            int x = rs.getInt("id");
+            String fio = rs.getString("fio");
+            double m = rs.getDouble("money");
+            System.out.println("получена строка: "+x+" "+fio+" "+m);
+        }
+        rs.close();
+        st.close();
+        conn.close();
     }
 
     public static void example0(){
@@ -24,6 +65,23 @@ public class Main {
         catch (SQLException e){
             System.out.println("все пропало");
             e.printStackTrace();
+        }
+    }
+
+    public static Connection connectToDB(){
+        String url = "jdbc:postgresql://10.10.104.136:5432/test4";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "123");
+        props.setProperty("ssl", "false");
+        try {
+            Connection conn = DriverManager.getConnection(url, props);
+            return conn;
+        }
+        catch (SQLException e){
+            System.out.println("все пропало");
+            e.printStackTrace();
+            return null;
         }
     }
 }
