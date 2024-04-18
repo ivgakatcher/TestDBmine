2b4e6726b6642fb5aa4971a99fb4f12356f78a09
ivgakatcher <itavr.working@gmail.com>
2024-04-18 12:37:34 +0300

    wreqwr

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,17 @@
 package org.example;
 
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Properties;
-import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-       // example0();
-        example1();
-        example2();
+        example10();
+       // example1();
+        //example2();
+        //exampleXXX();
+         example11XXX();
     }
 
     private static void example2() throws SQLException {
@@ -35,6 +37,8 @@ public class Main {
         st.close();
         return users;
     }
+
+
 
     private static void example1() throws SQLException {
         Connection conn = connectToDB();
@@ -84,4 +88,69 @@ public class Main {
             return null;
         }
     }
+    public static void exampleXXX(){
+        String url = "jdbc:postgresql://10.10.104.136:5432/test4";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "123");
+        props.setProperty("ssl", "false");
+        try {
+            Connection conn = DriverManager.getConnection(url, props);
+            System.out.println("удалось подключиться к БД Geometry");
+            conn.close();
+        }
+        catch (SQLException e){
+            System.out.println("все пропало");
+            e.printStackTrace();
+        }
+    }
+    private static void example1XXX() throws SQLException {
+        Connection conn = connectToDB();
+        Statement st = conn.createStatement();
+        String s = "SELECT * FROM public.\"Triangle\"";
+        ResultSet rs = st.executeQuery(s);
+        while (rs.next()) {
+            int x = rs.getInt("id");
+            int a = rs.getInt("a");
+            int b = rs.getInt("b");
+            int c = rs.getInt("c");
+            System.out.println("получена строка: "+x+" "+a+" "+b+" "+c);
+        }
+        rs.close();
+        st.close();
+        conn.close();
+    }
+    public static void example10(){
+        String url = "jdbc:postgresql://10.10.104.136:5432/test4";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "123");
+        props.setProperty("ssl", "false");
+        try {
+            Connection conn = DriverManager.getConnection(url, props);
+            System.out.println("удалось подключиться к БД test4");
+            conn.close();
+        }
+        catch (SQLException e){
+            System.out.println("все пропало");
+            e.printStackTrace();
+        }
+    }
+    private static void example11XXX() throws SQLException {
+        Connection conn = connectToDB();
+        Statement st = conn.createStatement();
+        String s = "SELECT * FROM public.message";
+        ResultSet rs = st.executeQuery(s);
+        while (rs.next()) {
+            int x = rs.getInt("id");
+            String a = rs.getString("text");
+            int b = rs.getInt("sender_id");
+            int c = rs.getInt("target_id");
+            System.out.println("получена строка: "+x+" "+a+" "+b+" "+c);
+        }
+        rs.close();
+        st.close();
+        conn.close();
+    }
+
 }
